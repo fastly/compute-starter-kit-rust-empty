58a6d7017c4e70f662240dd4b77c541139018669
Journie Ma-Johnson <journie.johnson@fastly.com>
2023-02-01 14:16:17 -0800

    Adding service version logging

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use fastly::{Error, Request, Response};
 
 #[fastly::main]
 fn main(_req: Request) -> Result<Response, Error> {
+    // Log service version
+    println!("FASTLY_SERVICE_VERSION: {}", std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_else(|_| String::new()));
+    
     Ok(Response::from_status(StatusCode::OK))
 }
